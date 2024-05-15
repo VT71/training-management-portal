d010e717091647db8d4db14502593477ec6fe006
VT71 <56217276+VT71@users.noreply.github.com>
2024-05-15 14:55:24 +0300

    Added Log In/Sign Up Buttons functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    constructor(private router: Router) {}
 
+    ngOnInit(): void {
+        const currentUrl = this.router.url;
+        console.log("Current URL: " + currentUrl);
+    }
 }
