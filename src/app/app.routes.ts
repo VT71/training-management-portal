a4c606bcb55a523d55ba10f6d9b869a387c3d352
VT71 <56217276+VT71@users.noreply.github.com>
2024-05-15 10:34:19 +0300

    Login Page Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Home page',
+  },
+];
