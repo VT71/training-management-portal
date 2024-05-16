2544eabfb36a6aebb763313de8ea73f6966a9dd7
VT71 <56217276+VT71@users.noreply.github.com>
2024-05-16 09:58:21 +0300

    Moved styling to global file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,35 +4,43 @@ import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TrainingsComponent } from './trainings/trainings.component';
 import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+const appName = 'Training Management Portal';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: '',
     component: HomeComponent,
-    title: 'Home page',
+    title: `Home | ${appName}`,
     children: [
       {
         path: 'login',
         component: LoginComponent,
-        title: 'Login page',
+        title: `Login | ${appName}`,
       },
       {
         path: 'register',
         component: RegisterComponent,
-        title: 'Register page',
+        title: `Register | ${appName}`,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+        title: `Forgot Password | ${appName}`,
       },
     ],
   },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    title: 'Dashboard page',
+    title: `Dashboard | ${appName}`,
     children: [
       {
         path: 'trainings',
         component: TrainingsComponent,
-        title: 'Register page',
+        title: `Trainings | ${appName}`,
       },
     ],
   },
