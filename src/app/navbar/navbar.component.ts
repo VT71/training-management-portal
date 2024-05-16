5d011269820543f76496dcfeb8cbca828745619c
VT71 <56217276+VT71@users.noreply.github.com>
2024-05-16 15:45:55 +0300

    Fixed badge display for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,11 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { NgIf } from '@angular/common';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatIconModule , MatMenuModule, MatMenuTrigger, NgIf],
+  imports: [ MatIconModule , MatMenuModule, MatMenuTrigger, NgIf, MatBadgeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
