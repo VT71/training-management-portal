22c6ea16615004df8bfa2f6a68b55980e10ab719
Dragan Sebastian <148530542+SebastianDragan@users.noreply.github.com>
2024-05-16 14:38:35 +0300

    nav bar update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatIconModule , MatMenuModule, MatMenuTrigger],
+  imports: [ MatIconModule , MatMenuModule, MatMenuTrigger, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+isDropdownOpen = false;
 
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    
+  }
 }
