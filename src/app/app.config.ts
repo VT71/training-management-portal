80355cef28b078c1f563f343773a501c5f15b8f2
Dragan Sebastian <148530542+SebastianDragan@users.noreply.github.com>
2024-05-17 11:27:42 +0300

    Resolve firebase bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,23 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideAuth, getAuth } from '@angular/fire/auth'
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBTAjOzOAqm_7ogl0k8F-hMIo-eM76--vU",
-  authDomain: "training-management-port-8a4b6.firebaseapp.com",
-  projectId: "training-management-port-8a4b6",
-  storageBucket: "training-management-port-8a4b6.appspot.com",
-  messagingSenderId: "1013225321299",
-  appId: "1:1013225321299:web:206cfdd9455dd36473a7a6",
-  measurementId: "G-53PMMH3PDT"
-};
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop'),
-    // importProvidersFrom([provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    //   provideAuth(() => getAuth()),
-    // ]),
-
-  ],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync('noop'),
     
-
+    provideFirebaseApp(() =>
+      initializeApp(
+       environment.firebaseConfig
+      )
+    ),
+    provideAuth(() => getAuth()),
+  ],
 };
