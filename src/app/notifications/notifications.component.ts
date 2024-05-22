9cf4e33f29600b615fc8e40565ff9b8ad9ffe14e
VT71 <56217276+VT71@users.noreply.github.com>
2024-05-22 14:45:15 +0300

    Customised notifications list with a different button

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
