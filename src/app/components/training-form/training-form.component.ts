f20183771e4be3720cc05f0360a6c6e71dc7dfa6
Dragan Sebastian <148530542+SebastianDragan@users.noreply.github.com>
2024-06-05 16:31:06 +0300

    dialog-form updated

--- src/app/components/training-form/training-form.component.ts
+++ src/app/components/training-form/training-form.component.ts
@@ -4,15 +4,20 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { ChipsAutocompleteComponent } from './chips-autocomplete/chips-autocomplete.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-training-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatDatepickerModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatDatepickerModule,  MatButtonModule, MatSelectModule, ChipsAutocompleteComponent, MatButtonToggleModule],
   templateUrl: './training-form.component.html',
   styleUrl: './training-form.component.css',
   providers: [provideNativeDateAdapter()],
 })
 export class TrainingFormComponent {
+
 
 }
