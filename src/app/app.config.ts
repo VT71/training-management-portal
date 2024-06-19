e1ac388b9d8ad960c327ebd703b2ae664cecfc7f
VT71 <56217276+VT71@users.noreply.github.com>
2024-06-19 12:47:49 +0300

    Employee data is now being received from the backend for the dynamic page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { routes } from './app.routes';
@@ -11,7 +11,7 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync('animations'),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
--- src/app/components/employee-page/employee-page.component.ts
+++ src/app/components/employee-page/employee-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Observable } from 'rxjs';
+import { EmployeeComplete } from '../../interfaces/employee-complete';
+import { EmployeesApiService } from '../../services/employees-api.service';
 
 @Component({
   selector: 'app-employee-page',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, AsyncPipe],
   templateUrl: './employee-page.component.html',
   styleUrl: './employee-page.component.css',
 })
-export class EmployeePageComponent {}
+export class EmployeePageComponent implements OnInit {
+  @Input() id!: number;
+
+  private employeeApiService = inject(EmployeesApiService);
+
+  public employeeData$!: Observable<EmployeeComplete>;
+
+  ngOnInit() {
+    if (this.id) {
+      this.employeeData$ = this.employeeApiService.getEmployeeComplete(this.id);
+    }
+  }
+}
