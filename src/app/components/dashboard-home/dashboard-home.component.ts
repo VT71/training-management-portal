9ddf3b65905d7f16ea9d5851e4372125fc54f5cf
VT71 <56217276+VT71@users.noreply.github.com>
2024-06-26 23:19:27 +0300

    Applying the filters now sets the heading with the date range

--- src/app/components/dashboard-home/dashboard-home.component.ts
+++ src/app/components/dashboard-home/dashboard-home.component.ts
@@ -11,11 +11,19 @@ import { GaugeChartComponent } from '../gauge-chart/gauge-chart.component';
 import { MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { AdvancedPieComponent } from '../advanced-pie/advanced-pie.component';
 import { GroupedVerticalBarChartComponent } from '../grouped-vertical-bar-chart/grouped-vertical-bar-chart.component';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ReportsApiService } from '../../services/reports-api.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import {
+  MatDateFormats,
+  MAT_NATIVE_DATE_FORMATS,
+} from '@angular/material/core';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -40,20 +48,43 @@ export class DashboardHomeComponent implements OnInit {
 
   completedTrainingsFromTotal$!: Observable<number>;
 
+  startDate: Date = new Date('2024-01-01T00:00:00.000');
+  endDate: Date = new Date('2024-12-31T00:00:00.000');
+
   readonly range = new FormGroup({
-    start: new FormControl<Date | null>(null, [Validators.required]),
-    end: new FormControl<Date | null>(null, [Validators.required]),
+    start: new FormControl<Date | null>(this.startDate, [Validators.required]),
+    end: new FormControl<Date | null>(this.endDate, [Validators.required]),
   });
 
   ngOnInit(): void {
-    this.completedTrainingsFromTotal$ =
-      this.reportsAPiService.getPercentageOfCompletedTrainingsByRange(
-        '2023-01-01T09:26:40.577',
-        '2028-01-01T09:26:40.577'
-      );
+    const rawForm = this.range.getRawValue();
+    if (rawForm.start && rawForm.end) {
+      this.completedTrainingsFromTotal$ =
+        this.reportsAPiService.getPercentageOfCompletedTrainingsByRange(
+          rawForm.start.toISOString(),
+          rawForm.end.toISOString()
+        );
+    }
   }
 
   public onSubmit() {
-    console.log('DATE: ' + new Date().toISOString());
+    if (this.range.valid) {
+      const rawForm = this.range.getRawValue();
+      if (rawForm.start && rawForm.end) {
+        rawForm.start.setDate(rawForm.start.getDate() + 1);
+        rawForm.end.setDate(rawForm.end.getDate() + 1);
+
+        this.startDate = new Date(rawForm.start?.toISOString()?.slice(0, 10));
+        this.endDate = new Date(rawForm.end?.toISOString()?.slice(0, 10));
+      }
+    }
+  }
+
+  public convertDate(date: Date | null): string {
+    if (!date) {
+      return '';
+    }
+
+    return date?.toISOString()?.slice(0, 10)?.split('-')?.reverse().join('/');
   }
 }
